aa3eaae0cbfd84301c24548b995ae75ec6f122ad
liwei_PC <15516523738@163.com>
2019-01-26 11:21:11 +0800

    000

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
@@ -8,5 +9,12 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/user/<path:id>')
+def user(id):
+    return '<h1>Hello,%s...</h1>' %id
+
+
 if __name__ == '__main__':
-    app.run()
+
+    #print(__name__)
+    app.run(debug=True)
